a7326d461252fb4edaa7d5966eba86835704be46
deepak_alfa <deepakalphawizz@gmail.com>
2022-02-04 13:39:38 +0530

    main

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,18 @@
+final String appName = 'Axomia User';
+
+final String packageName = 'com.alpha.ecommerce';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.alpha.ecommerce';
+final String iosLink = 'your ios link here';
+final String appStoreId = '123456789';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+final String baseUrl = 'https://alphawizztest.tk/Axomia/app/v1/api/';
+
+final String jwtKey = "78084f1698c9fcff5a668b68dcd103db39be2605";
